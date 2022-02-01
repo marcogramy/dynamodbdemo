ac834bf717c94a09703f31bb5b1853ebbf85d811
Marco Cozzi <marco@codeatlas.it>
2022-02-01 09:25:52 +0100

    Add intermediate model for testing purposes

--- src/main/java/com/example/dynamodbdemo/model/Entity.java
+++ src/main/java/com/example/dynamodbdemo/model/Entity.java
@@ -4,35 +4,25 @@ import com.example.dynamodbdemo.converter.CustomAttributeConverterProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DefaultAttributeConverterProvider;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 
-import java.util.Optional;
-
 @DynamoDbBean(converterProviders = {
         CustomAttributeConverterProvider.class,
         DefaultAttributeConverterProvider.class})
-public class Entity extends BaseEntity {
+public class Entity extends MiddleEntity {
 
     private final Long customerId;
-    private final Long applicationId;
-    private final String text;
 
     public Entity() {
         customerId = null;
-        applicationId = null;
-        text = null;
     }
 
-    public Entity(String contextId, String type, Long customerId, Long applicationId, String text) {
-        super(contextId, type);
+    public Entity(String contextId, String type, Long customerId, Long applicationId, String notificationText) {
+        super(contextId, type, applicationId, notificationText);
         this.customerId = customerId;
-        this.applicationId = applicationId;
-        this.text = text;
     }
 
     public Entity(String contextId, String type, Long customerId, Long applicationId) {
-        super(contextId, type);
+        super(contextId, type, applicationId);
         this.customerId = customerId;
-        this.applicationId = applicationId;
-        text = null;
     }
 
     public Long getCustomerId() {
@@ -43,19 +33,5 @@ public class Entity extends BaseEntity {
         // Do nothing, this is a derived attribute
     }
 
-    public Optional<Long> getApplicationId() {
-        return Optional.ofNullable(applicationId);
-    }
 
-    public void setApplicationId(Optional<Long> applicationId) {
-        // Do nothing, this is a derived attribute
-    }
-
-    public Optional<String> getText() {
-        return Optional.ofNullable(text);
-    }
-
-    public void setText(Optional<String> text) {
-        // Do nothing, this is a derived attribute
-    }
 }
---
+++ src/main/java/com/example/dynamodbdemo/model/MiddleEntity.java
@@ -0,0 +1,67 @@
+package com.example.dynamodbdemo.model;
+
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+
+import java.time.Instant;
+import java.util.Optional;
+
+@DynamoDbBean
+public class MiddleEntity extends BaseEntity {
+
+    private final Long applicationId;
+    private final String text;
+    private final String notificationText;
+    private final Instant deliveredAt;
+
+    public MiddleEntity() {
+        this.deliveredAt = null;
+        applicationId = null;
+        this.text = null;
+        notificationText = null;
+    }
+
+    public MiddleEntity(String contextId, String type, Long applicationId) {
+        super(contextId, type);
+        this.applicationId = applicationId;
+        this.deliveredAt = null;
+        this.text = "text";
+        this.notificationText = null;
+    }
+
+    public MiddleEntity(String contextId, String type, Long applicationId, String notificationText) {
+        super(contextId, type);
+        this.applicationId = applicationId;
+        this.text = "text";
+        this.notificationText = notificationText;
+        this.deliveredAt = null;
+    }
+
+    public Optional<Long> getApplicationId() {
+        return Optional.ofNullable(applicationId);
+    }
+
+    public void setApplicationId(Optional<Long> applicationId) {
+        // Do nothing, this is a derived attribute
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) { } // Do nothing, this is a derived attribute
+
+    public Optional<String> getNotificationText() {
+        return Optional.ofNullable(notificationText);
+    }
+
+    public void setNotificationText(Optional<String> notificationText) {
+        // Do nothing, this is a derived attribute
+    }
+
+    public Optional<Instant> getDeliveredAt() {
+        return Optional.ofNullable(deliveredAt);
+    }
+
+    public void setDeliveredAt(Optional<Instant> deliveredAt) { } // Do nothing, this is a derived attribute
+
+}
